19e8e536c3c9be4df4eea26784e3e132c9713d88
SunnyTong2019 <50933364+SunnyTong2019@users.noreply.github.com>
2019-06-25 11:29:16 -0700

    Added helmet.xssFilter

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
+var helmet = require('helmet');
+
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -19,6 +21,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet.xssFilter());
 
 //Sample front-end
 app.route('/:project/')
